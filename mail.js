81f388a85f675d3c15b3af97febf79e6071831a6
KristynaBartonova <kikisbartonova@gmail.com>
2020-12-03 22:30:49 +0100

    mail.jsChange

--- mail.js
+++ mail.js
@@ -1,19 +1,19 @@
-let myFunction =() => {
-  let x = document.getElementById("frm1");
-  let email = document.getElementById("email");
-  let zprava = document.getElementById("zprava");
-  let text = "";
-  let i;
-  for (i = 0; i < x.length ;i++) {
-    text += x.elements[i].value + "<br>";
-  }
-  event.preventDefault()
- console.log(`email:${email.value}` + `zprava:${zprava.value}`);
-  x.reset();
-};
+// let myFunction =() => {
+//   let x = document.getElementById("frm1");
+//   let email = document.getElementById("email");
+//   let zprava = document.getElementById("zprava");
+//   let text = "";
+//   let i;
+//   for (i = 0; i < x.length ;i++) {
+//     text += x.elements[i].value + "<br>";
+//   }
+//   event.preventDefault()
+//  console.log(`email:${email.value}` + `zprava:${zprava.value}`);
+//   x.reset();
+// };
 
-// var $form = $('form#test-form'),
-//     url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
+//  var $form = $('form#frm1'),
+//  url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
 
 // $('#submit-form').on('click', function(e) {
 //   e.preventDefault();
@@ -27,4 +27,13 @@ let myFunction =() => {
 //   );
 // })
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycby13YWyYR6_nPYk6awJwvSeVlKWNFia9jNmIAWBuY9m_elEBwwN/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
 
