a48c9b5a56d5d85357d82b946cfcee40f976cf24
Barbora Petříková <bb.petrikova@gmail.com>
2020-12-06 19:04:13 +0100

    zkouška kotvy

--- index.js
+++ index.js
@@ -416,7 +416,7 @@ const printResults = () => {
     const anchor = document.createElement('a');
     anchor.innerHTML = `Chci vědět víc`;
     anchor.classList.add('final-btn');
-    anchor.href = `infodemie/content.html/#${topic.topic}`;
+    anchor.href = `/content.html#${topic.topic}`;
     // vytvářím si odstavec, který se vypíše na konečné stránce
     const score = document.createElement('p');
     score.classList.add('score');
